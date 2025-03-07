19618b62bc51375181865395b9542d5e70006558
EssghaierJacem <jacemsghaier3@gmail.com>
2025-03-08 00:49:12 +0100

    Backend initialization

---
+++ elvitrina-backend/src/main/java/com/sudoers/elvitrinabackend/ElvitrinaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sudoers.elvitrinabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElvitrinaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElvitrinaBackendApplication.class, args);
+    }
+
+}
---
+++ elvitrina-backend/src/test/java/com/sudoers/elvitrinabackend/ElvitrinaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudoers.elvitrinabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElvitrinaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
