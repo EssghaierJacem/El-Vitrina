60280782df613e2f34610163a5ee88c2452a91b0
EssghaierJacem <jacemsghaier3@gmail.com>
2025-03-08 03:24:07 +0100

    AppFeedBack:Entity update on constructor

--- elvitrina-backend/src/main/java/com/sudoers/elvitrinabackend/model/entity/AppFeedback.java
+++ elvitrina-backend/src/main/java/com/sudoers/elvitrinabackend/model/entity/AppFeedback.java
@@ -1,13 +1,10 @@
 package com.sudoers.elvitrinabackend.model.entity;
 
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class AppFeedback extends Feedback {
 }
