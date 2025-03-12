daa6e368a81aca8183783528ca412df06a08af77
meriem277 <meriem.rehouma@esprit.tn>
2025-03-12 05:16:05 +0100

    update crud payment & Quizz

--- elvitrina-backend/src/main/java/com/sudoers/elvitrinabackend/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sudoers.elvitrinabackend.repository;
-import com.sudoers.elvitrinabackend.model.entity.CustomOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<CustomOrder, Long> {
-}
