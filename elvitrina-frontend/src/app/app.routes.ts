6628e1fc6c9f937bcb1cd1027bf551c2a25a1421
inesmrad11 <147980758+inesmrad11@users.noreply.github.com>
2025-04-04 13:38:33 +0100

    Update routing configuration to include offers and authentication paths
    - Added a new route for offers with lazy loading of the corresponding module.
    - Introduced a route for authentication, also utilizing lazy loading for improved performance.
    - Removed redundant empty child route under the FrontComponent.

--- elvitrina-frontend/src/app/app.routes.ts
+++ elvitrina-frontend/src/app/app.routes.ts
@@ -2,14 +2,26 @@ import { Routes } from '@angular/router';
 import { BlankComponent } from './layouts/blank/blank.component';
 import { FrontComponent } from './layouts/frontoffice/front.component';
 import { FullComponent } from './layouts/full/full.component';
-import { UserTableComponent } from './main-components/user/user-table/user-table.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: FrontComponent, 
-    children: [    
+    component: FrontComponent,
+    children: [
+      {
+        path: 'offers',
+        loadChildren: () =>
+          import('./main-components/offer/frontoffice/frontoffice_offer.routes').then((m) => m.FrontOfferRoutes),
+      },
     ],
+  },
+  {
+    path: 'authentication',
+    component: BlankComponent,
+    loadChildren: () =>
+      import('./pages/authentication/authentication.routes').then(
+        (m) => m.AuthenticationRoutes
+      ),
   },
   {
     path: 'dashboard',
@@ -66,11 +78,6 @@ export const routes: Routes = [
           import('./pages/extra/extra.routes').then((m) => m.ExtraRoutes),
       },
     ],
-  },
-  {
-    path: '',
-    component: FrontComponent,
-    children: [],
   },
   {
     path: '**',
