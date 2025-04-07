58c43ae55538654e460761ccb2369c627a74e781
EssghaierJacem <jacemsghaier3@gmail.com>
2025-04-07 13:37:58 +0100

    Become Seller

--- elvitrina-frontend/src/app/main-components/user/frontoffice/frontuser.routes.ts
+++ elvitrina-frontend/src/app/main-components/user/frontoffice/frontuser.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { ChangePasswordComponent } from './user-change-password/user-change-password.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { BecomeSellerComponent } from './become-seller/become-seller.component';
 
 export const FrontUserRoutes: Routes = [
   {path: 'list',component: UserListComponent},
+  {path: 'become-seller',component: BecomeSellerComponent},
   {path: ':id/profile',component: UserProfileComponent},
   {path: 'change-password',component: ChangePasswordComponent},
 ];
